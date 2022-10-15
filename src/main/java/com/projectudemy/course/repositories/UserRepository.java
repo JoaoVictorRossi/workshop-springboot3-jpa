70de706dd3cea0ce4680e04c0b7aa904a92a3020
João Victor Rossi <jvrossi204@gmail.com>
2022-10-15 17:15:30 -0300

    Adding Componet Spring (Service) UserService

--- src/main/java/com/projectudemy/course/repositories/UserRepository.java
+++ src/main/java/com/projectudemy/course/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.projectudemy.course.entities.User;
 
+
 public interface UserRepository extends JpaRepository<User, Long>{
 
 }
